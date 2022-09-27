0c2953e473ac3215799a5ea6f80fa5482a612f49
Helio Vieira <helio.engenharia@gmail.com>
2022-09-26 22:09:46 -0300

    Initial Commit

---
+++ MCommerce_SwiftUI/MCommerce_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCommerce_SwiftUIApp.swift
+//  MCommerce_SwiftUI
+//
+//  Created by Helio Junior on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MCommerce_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
